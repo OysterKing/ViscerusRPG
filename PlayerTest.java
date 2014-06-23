b37573945c194eda9c784d12b1c8925b5b38e3aa
OysterKing <comhghall.mc-keating@ucd.ie>
2014-06-23 14:28:41 +0100

    Crude outline of a player class and its associated objects.

---
+++ PlayerTest.java
@@ -0,0 +1,28 @@
+
+public class PlayerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Player player = new Player();
+		
+		
+		
+		System.out.println("Name:\t" + player.characterBasics.getName());
+		System.out.println("Age:\t" + player.characterBasics.getAge());
+		System.out.println("Race:\t" + player.characterBasics.getRace());
+		System.out.println("Height:\t" + player.characterBasics.getHeight());
+		System.out.println("Weight:\t" + player.characterBasics.getWeight());
+		System.out.println("Eye Colour:\t" + player.characterBasics.getEye_colour());
+		System.out.println("Hair Colour:\t" + player.characterBasics.getHair_colour());
+		System.out.println();
+		
+		System.out.println("ABILITIES:");
+		player.printAbilities();
+		System.out.println();
+		System.out.println("SKILLS:");
+		player.printSkills();
+
+	}
+
+}
